8b9820faefc4251d2c585d8379b22a01633abdb7
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-09-11 16:27:47 -0400

    first commit

---
+++ src/main/java/com/school/dinosaur_api/DinosaurApiApplication.java
@@ -0,0 +1,13 @@
+package com.school.dinosaur_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinosaurApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinosaurApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/dinosaur_api/DinosaurApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.school.dinosaur_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosaurApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello World");
+	}
+
+}

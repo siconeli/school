0b1e3364fd118a4660d9500c46eb7498512d5b0d
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-09-19 17:26:31 -0400

    refatorando arquivo sql do flyway para criar as tabelas

--- src/main/resources/db/migration/V001__create-student-table.sql
+++ src/main/resources/db/migration/V001__create-student-table.sql
@@ -1,11 +1,6 @@
 create table student (
-     id bigserial not null,
-     cpf varchar(11) not null,
+     id bigserial primary key not null,
+     cpf varchar(11) unique not null,
      name varchar(60) not null,
-     age int not null,
-
-     primary key (id)
+     age int not null
 );
-
-alter table student
-    add constraint uk_cpfStudent unique(cpf);

7022877cbf3292c75061f76fc8015e7a1ffde6eb
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-09-23 14:45:53 -0400

    implementando o ProblemDetail RFC 7807

--- src/main/java/com/school/dinosaur_api/api/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/school/dinosaur_api/api/exceptionhandler/GlobalExceptionHandler.java
@@ -2,13 +2,28 @@ package com.school.dinosaur_api.api.exceptionhandler;
 
 import com.school.dinosaur_api.domain.exception.BusinessException;
 import com.school.dinosaur_api.domain.exception.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // IMPLEMENTANDO O PLOBLEMDETAIL - RFC 7807
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(URI.create("https://api.dinosaur/erros/"));
+        problemDetail.setTitle("Erro em um dos campos");
+        problemDetail.setDetail("O campo x está vazio");
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<String> captureException(BusinessException e) {

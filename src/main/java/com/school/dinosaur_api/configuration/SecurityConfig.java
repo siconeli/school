908c5004d48ef4de9b413aeb20ee5b275141b9f5
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-10-09 15:44:44 -0400

    up

--- src/main/java/com/school/dinosaur_api/configuration/SecurityConfig.java
+++ src/main/java/com/school/dinosaur_api/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize ->
                     authorize.requestMatchers(HttpMethod.PUT, "/responsibles/{responsibleId}/authorize").hasRole("ADMIN")

122ce05edeff30b2ddd60de2e27b943c67b28dc1
siconeli <henrique.siconeli.f@hotmail.com>
2024-10-13 06:18:20 -0400

    Implementei o UserController

---
+++ src/main/java/com/school/dinosaur_api/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.school.dinosaur_api.api.controller;
+
+import com.school.dinosaur_api.api.representationmodel.input.CreateUserDTO;
+import com.school.dinosaur_api.api.representationmodel.input.JwtTokenDTO;
+import com.school.dinosaur_api.api.representationmodel.input.LoginUserDTO;
+import com.school.dinosaur_api.domain.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtTokenDTO> loginUser(@RequestBody LoginUserDTO loginUserDTO) {
+        JwtTokenDTO token = userService.authenticateUser(loginUserDTO);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDTO createUserDTO) {
+        userService.saveUser(createUserDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/school/dinosaur_api/domain/service/UserService.java
+++ src/main/java/com/school/dinosaur_api/domain/service/UserService.java
@@ -1,12 +1,17 @@
 package com.school.dinosaur_api.domain.service;
 
 import com.school.dinosaur_api.api.representationmodel.input.CreateUserDTO;
+import com.school.dinosaur_api.api.representationmodel.input.JwtTokenDTO;
+import com.school.dinosaur_api.api.representationmodel.input.LoginUserDTO;
 import com.school.dinosaur_api.domain.model.ModelRole;
+import com.school.dinosaur_api.domain.model.ModelUserDetails;
 import com.school.dinosaur_api.domain.model.User;
 import com.school.dinosaur_api.domain.repository.UserRepository;
 import com.school.dinosaur_api.domain.security.SecurityConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,7 +27,7 @@ public class UserService {
 
     private JwtTokenService jwtTokenService;
 
-    public void salvarUsuario(CreateUserDTO createUserDTO) {
+    public void saveUser(CreateUserDTO createUserDTO) {
         User newUser = User.builder()
                 .name(createUserDTO.name())
                 .password(securityConfig.passwordEncoder().encode(createUserDTO.password()))
@@ -30,5 +35,14 @@ public class UserService {
                 .build();
 
         userRepository.save(newUser);
+    }
+
+    public JwtTokenDTO authenticateUser(LoginUserDTO loginUserDTO) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginUserDTO.login(), loginUserDTO.password());
+
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        ModelUserDetails modelUserDetails = (ModelUserDetails) authentication.getPrincipal();
+
+        return new JwtTokenDTO(jwtTokenService.generateToken(modelUserDetails));
     }
 }
